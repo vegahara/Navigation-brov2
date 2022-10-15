c06c210e252a361c4454d2ee31ee0448e01fad6c
Vegard Haraldstad <vegard.haraldstad@gmail.com>
2022-10-15 11:11:54 +0200

    Added populate fcn forsonar data

--- utility_functions/csv_2_rosbag.py
+++ utility_functions/csv_2_rosbag.py
@@ -11,6 +11,8 @@ from nav_msgs.msg import Odometry
 
 from utility_functions import quaternion_from_euler
 
+import matplotlib.pyplot as plt
+
 
 def get_rosbag_options(path, serialization_format='cdr'):
     storage_options = rosbag2.StorageOptions(uri=path, storage_id='sqlite3')
@@ -32,6 +34,8 @@ def create_topic(writer, topic_name, topic_type, serialization_format='cdr'):
         
 def convert_data(writer, df_arr, topic_name_arr, msg_type_arr, populate_fcn_arr):
     for df, topic_name, msg_type, populate_fcn in zip(df_arr, topic_name_arr, msg_type_arr, populate_fcn_arr): 
+        bad_msgs = 0
+        good_msgs = 0
         for row in range(df.shape[0]):
             msg = msg_type()
 
@@ -39,24 +43,41 @@ def convert_data(writer, df_arr, topic_name_arr, msg_type_arr, populate_fcn_arr)
             time_stamp = Time(seconds = seconds_int, nanoseconds = seconds_decimal * (10 ** 9))
 
             msg = msg_type()
-            msg = populate_fcn(msg, df, row) 
+            msg, msg_good = populate_fcn(msg, df, row) 
             msg.header.stamp = time_stamp.to_msg()    
             
-            writer.write(topic_name, serialize_message(msg), time_stamp.nanoseconds)
+            if msg_good:
+                good_msgs += 1
+                writer.write(topic_name, serialize_message(msg), time_stamp.nanoseconds)
+            else:
+                bad_msgs += 1
 
-        print("Data with topic name ", topic_name, " is written to bag")    
+        print("Data with topic name ", topic_name, " is written to bag")
+        print("Wrote ", good_msgs, " messages to the bag")
+        print("Got ", bad_msgs, " bad messages from the populate fcn")   
 
 def populate_sonar_msg(sonar_msg, df, row):
+    # Just discarding last value in data for now, should be investigated
+    # Some messages has only 500 bytes and gets discarded
+    
     data = df[' data'][row]
+    sonar_data = []
+    
+    for i in range(0, len(data), 2):
+        sonar_data.append(int(data[i:i+2], base=16))
 
-    # populate msg..
+    if (len(sonar_data) != 2001):
+        return sonar_msg, False
+    else:
+        sonar_msg.data_zero = [int_val.to_bytes(2, 'big') for int_val in sonar_data[:len(sonar_data)//2]]
+        sonar_msg.data_one = [int_val.to_bytes(2, 'big') for int_val in sonar_data[len(sonar_data)//2:-1]]
 
-    return sonar_msg
+    return sonar_msg, True
 
 def populate_dvl_msg(dvl_msg, df, row):
     dvl_msg.altitude  = df[' alt (m)'][row]
 
-    return dvl_msg
+    return dvl_msg, True
 
 def populate_odom_msg(odom_msg, df, row):
     phi = df[' phi (rad)'][row]
@@ -75,11 +96,11 @@ def populate_odom_msg(odom_msg, df, row):
     odom_msg.pose.pose.position.y = df[' y (m)'][row]
     odom_msg.pose.pose.position.z = df[' z (m)'][row]
 
-    return odom_msg
+    return odom_msg, True
 
 def main():
     # Set up bag
-    bag_path = 'bags/test_bags'
+    bag_path = 'bags/test_sonar'
 
     storage_options, converter_options = get_rosbag_options(bag_path)
 
@@ -101,10 +122,10 @@ def main():
     df_odom = pd.read_csv('bags/EstimatedState.csv')
 
     convert_data(writer,
-                [df_dvl, df_odom],  
-                [dvl_topic_name, odom_topic_name], 
-                [DVL, Odometry], 
-                [populate_dvl_msg, populate_odom_msg]
+                [df_dvl, df_odom, df_sonar],  
+                [dvl_topic_name, odom_topic_name, sonar_topic_name], 
+                [DVL, Odometry, Sonar], 
+                [populate_dvl_msg, populate_odom_msg, populate_sonar_msg]
     )
 
     return 0
