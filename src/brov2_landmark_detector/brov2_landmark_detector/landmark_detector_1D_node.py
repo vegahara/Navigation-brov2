6ec652a06a7630951cd156b0db89f2d2570e2283
Vegard Haraldstad <vegard.haraldstad@gmail.com>
2022-10-18 21:40:34 +0200

    Added possibility for plotting of swath and landmarks

--- src/brov2_landmark_detector/brov2_landmark_detector/landmark_detector_1D_node.py
+++ src/brov2_landmark_detector/brov2_landmark_detector/landmark_detector_1D_node.py
@@ -1,7 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.signal import find_peaks, peak_prominences, peak_widths
 from math import pi, floor
 from typing import List
+from csaps import csaps
+
 
 from rclpy.node import Node
 
@@ -36,26 +39,28 @@ class SideScanSonar:
         self.theta = sensor_angle_placement
         self.alpha = sensor_opening
 
-    
-class LandmarkDetector1D(Node):
 
-    # def __init__(self, sonar: SideScanSonar, threshold: float = 10):
-    #     self.shadow_landmarks = []                      # Containing all detected shadow landmarks 
-    #     self.echo_landmarks = []                        # Containing all detected echo landmarks   
-    #     self.sonar = sonar 
-    #     self.landmark_threshold = threshold             # Arbitrary threshold for which landmarks to throw away
+class LandmarkDetector1D(Node):
 
     def __init__(self):
         super().__init__('landmark_detector')
 
         self.declare_parameters(namespace='',
             parameters=[('sonar_data_topic_name', 'sonar_processed'),
-                        ('landmark_detector_threshold', 10)]
+                        ('landmark_detector_threshold', 10),
+                        ('n_samples', 1000),
+                        ('cubic_spl_smoothing_param', 1e-6)]
         )
                       
-        sonar_data_topic_name, self.landmark_threshold = self.get_parameters([
+        (sonar_data_topic_name, 
+        self.landmark_threshold, 
+        self.n_samples, 
+        self.cubic_spl_smoothing_param) = \
+        self.get_parameters([
             'sonar_data_topic_name', 
-            'landmark_detector_threshold'
+            'landmark_detector_threshold',
+            'n_samples',
+            'cubic_spl_smoothing_param'
         ])
                                                                             
         self.sonar_processed_subscription = self.create_subscription(
@@ -67,8 +72,8 @@ class LandmarkDetector1D(Node):
         self.sonar_processed_subscription # prevent unused variable warning
         
         # Landmark detection - initialization
-        self.shadow_landmarks = []                      # Containing all detected shadow landmarks 
-        self.echo_landmarks = []                        # Containing all detected echo landmarks   
+        self.shadow_landmarks = []  # Containing all detected shadow landmarks 
+        self.echo_landmarks = []    # Containing all detected echo landmarks   
 
         self.get_logger().info("Landmark detector node initialized.")
 
@@ -92,6 +97,9 @@ class LandmarkDetector1D(Node):
         self.find_landmarks(swath)
 
     def find_landmarks(self, swath: Swath):
+        
+        swath.swath_port = self.swath_smoothing(swath.swath_port)
+        swath.swath_stb = self.swath_smoothing(swath.swath_stb)
 
         # Find all properties of swath. Make sure swath are flipped right way
         # To find shadows, swath is flipped
@@ -105,8 +113,10 @@ class LandmarkDetector1D(Node):
         echo_properties.extend(self.find_swath_properties(np.flip(swath.swath_port)))
         echo_properties.extend(self.find_swath_properties(swath.swath_stb))
 
-        _shadow_landmarks = self.extract_landmarks(swath, shadow_properties)
-        _echo_landmarks = self.extract_landmarks(swath, echo_properties)
+        shadow_landmarks = self.extract_landmarks(swath, shadow_properties)
+        echo_landmarks = self.extract_landmarks(swath, echo_properties)
+
+        self.plot_landmarks(swath, echo_landmarks, shadow_landmarks)
 
         # How to handle landmark detection when we detect both shadows and echoes? Should they be matched up?
 
@@ -121,7 +131,10 @@ class LandmarkDetector1D(Node):
         peaks = np.delete(peaks, 0)
 
         prominences, left_bases, right_bases = peak_prominences(swath, peaks)
-        widths, _, _, _ = peak_widths(swath, peaks, 0.5, (prominences, left_bases, right_bases))
+
+        widths, _, _, _ = peak_widths(
+            swath, peaks, 0.5, (prominences, left_bases, right_bases)
+        )
 
         swath_properties = []
 
@@ -131,12 +144,18 @@ class LandmarkDetector1D(Node):
         return swath_properties
 
     def extract_landmarks(self, swath: Swath, swath_properties):
-        landmarks = np.zeros((1,len(swath.swath_port) + len(swath.swath_stb)))
-
+        landmarks = [0] * (len(swath.swath_port) + len(swath.swath_stb))
+        
         for (peak, width, prominence) in swath_properties:
+
             if (2 * width) / prominence < self.landmark_threshold.value:
-                self.shadow_landmarks.append(Landmark(self.get_global_pos(swath, peak), width, prominence))
-                landmarks[peak - floor(width/2):peak + floor(width/2)] = 1
+                self.shadow_landmarks.append(Landmark(
+                    self.get_global_pos(swath, peak), 
+                    width, 
+                    prominence
+                ))
+                landmarks[peak - floor(width/2):peak + floor(width/2)] = \
+                    [1] * floor(width)
 
         return landmarks
 
@@ -153,3 +172,37 @@ class LandmarkDetector1D(Node):
     # Not implemented
     def get_global_pos(self, swath: Swath, peak: int):
         return (0, 0, 0)
+
+    def plot_landmarks(self, swath: Swath, echo_landmarks, shadow_landmarks):
+        
+        swath_array = swath.swath_port + swath.swath_stb
+
+        print(swath_array)
+
+        for i in range(len(swath_array)):
+
+            if (echo_landmarks[i] == 0):
+                echo_landmarks[i] = np.nan
+            else:
+                echo_landmarks[i] = echo_landmarks[i] * swath_array[i]
+                swath_array[i] = np.nan
+
+            if (shadow_landmarks[i] == 0):
+                shadow_landmarks[i] = np.nan
+            else:
+                shadow_landmarks[i] = shadow_landmarks[i] * swath_array[i]
+                swath_array[i] = np.nan                
+
+        plt.plot(swath_array, color='b')
+        plt.plot(shadow_landmarks, color='y')
+        plt.plot(echo_landmarks, color='g')
+
+        plt.show()
+
+        input("Press key to continue")
+
+    def swath_smoothing(self, swath):
+        x = np.linspace(0., self.n_samples.value, self.n_samples.value)
+        spl = csaps(x, swath, x, smooth=self.cubic_spl_smoothing_param.value)
+
+        return spl            
